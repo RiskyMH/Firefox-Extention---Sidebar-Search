0354472148452d0e8f6fc13b89e9789879f71898
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2019-10-08 21:41:28 +1100

    Update to use async/await
    And spelling

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
-/*
-Create a menu item for each installed search engine.
-The ID and title are both set to the search engine's name.
-*/
-function createMenuItem(engines) {
-    for (let engine of engines) {
+// Self executing async so async/await can be used.
+(async () => {
+    /* Create a menu item for each installed search engine. 
+    The ID and title are both set to the search engine's name.
+    */
+    const engines = await browser.search.get()
+    engines.forEach(engine => {
         browser.menus.create({
             id: engine.name,
             title: engine.name,
@@ -12,19 +13,13 @@ function createMenuItem(engines) {
             },
             contexts: ["selection"]
         });
-    }
-}
-
-browser.search.get().then(createMenuItem);
-
-console.log(createMenuItem)
-/*
-Search using the search engine whose name matches the
-menu item's ID.
-*/
-browser.menus.onClicked.addListener((info, tab) => {
-    browser.search.search({
-        query: info.selectionText,
-        engine: info.menuItemId
     });
-});
+
+    // Search using the search engine whose name matches the menu item's ID.
+    browser.menus.onClicked.addListener(info => {
+        browser.search.search({
+            query: info.selectionText,
+            engine: info.menuItemId
+        });
+    });
+})()
